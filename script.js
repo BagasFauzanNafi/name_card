d60d7f0543f85cc5cb78060242737daa0ae6aa53
unknown <bagasnafi19@gmail.com>
2024-09-19 21:14:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle Menu on Small Screens
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
